0066fab284ddeb868977b211abb37b20c8529f71
Muhammad Abu Dzar Al Ghifari <ghifaracc@gmail.com>
2026-05-08 17:32:41 +0700

    1

---
+++ Student17.java
@@ -0,0 +1,15 @@
+public class Student17 {
+    String nim, name, major;
+
+    Student17(String nim, String name, String major) {
+        this.nim = nim;
+        this.name = name;
+        this.major = major;
+    }
+
+    void print() {
+        System.out.println("NIM    : " + nim);
+        System.out.println("Name   : " + name);
+        System.out.println("Major  : " + major);
+    }
+}
